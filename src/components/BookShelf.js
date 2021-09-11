54c51c387faeb52d579c41b4a479dbc33f23de2e
Tim Grein <tim@4greins.de>
2021-09-11 09:29:02 +0200

    Change book key in BookShelf

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -9,7 +9,7 @@ class BookShelf extends React.Component {
                 <div className="bookshelf-books">
                     <ol className="books-grid">
                         {this.props.books.map((book) => (
-                            <li key={book.title}>
+                            <li key={book.id}>
                                 <Book book={book}
                                       onBookShelfChange={this.props.onBookShelfChange}/>
                             </li>
