f9ad2555fa100237ef6ca5740fd33c5ac568a5f2
Tim Grein <tim@4greins.de>
2021-09-11 09:13:31 +0200

    Add BookShelf, which groups several books together.

---
+++ src/components/BookShelf.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Book from "./Book";
+
+class BookShelf extends React.Component {
+    render() {
+        return (
+            <div className="bookshelf">
+                <h2 className="bookshelf-title">{this.props.title}</h2>
+                <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        {this.props.books.map((book) => (
+                            <li key={book.title}>
+                                <Book book={book}
+                                      onBookShelfChange={this.props.onBookShelfChange}/>
+                            </li>
+                        ))}
+                    </ol>
+                </div>
+            </div>
+        )
+
+    }
+}
+
+export default BookShelf
